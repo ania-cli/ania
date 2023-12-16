10603e11199544f411875844a1ecf5b2ec151ea9
Miguel <miguesdev@gmail.com>
2023-12-15 20:03:24 -0500

    refactor: configured to build

--- ania/__init__.py
+++ ania/__init__.py
@@ -1,19 +1,11 @@
-from .ui import clear, get_input, check_conection
-from .search_anime import search_anime
-from .select_anime import select_anime
-from .select_episode import select_episode
-from .get_servers import get_servers
-from .web_player import web_player
+from .app  import ania
+from ania.ui import error, bye
 
-def ania():
-  """
-  Ejecuta la aplicación Ania para buscar y seleccionar animes.
-  """
-  clear()
-  check_conection()
-  search = get_input('Buscar: ')
-  animes = search_anime(search)
-  anime = select_anime(animes, search)
-  animei = select_episode(anime)
-  servers = get_servers(animei)
-  web_player(animei, servers)
+def run():
+    try:
+        ania()
+
+    except KeyboardInterrupt:
+        bye()
+#    except Exception as e:
+#        error(e)
