209101dd777a78edf8ec355a11f094e38e7bc772
Miguel <miguesdev@gmail.com>
2023-12-28 17:00:33 -0500

    feat: flask implemented

--- ania/web_player/__init__.py
+++ ania/web_player/__init__.py
@@ -1,46 +1,32 @@
-import http.server
-import socketserver
-from jinja2 import Environment, FileSystemLoader, select_autoescape
 from ania.ui import printb, clear, bye
 from termcolor import colored
-
-port = 5666
-handler = http.server.SimpleHTTPRequestHandler
-
-env = Environment(
-    loader=FileSystemLoader('ania/web_player/templates'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+from flask import Flask, render_template, request, redirect
 
 def web_player(animei, servers):
-  class Router(handler):
-    def do_GET(self):
-      if self.path == '/':
-        template = env.get_template("index.html")
-        content = template.render({'title': animei.title, 'servers': servers, 'episode': animei.episode})
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(content.encode('utf-8'))
-      
-      elif self.path != '/':
-        self.send_response(302)
-        self.send_header('Location', '/')
-        self.end_headers()    
- 
-  with socketserver.TCPServer(("", port), Router) as httpd:
-    episode = colored(str(animei.episode), 'cyan', attrs=['bold'])
-    title = colored(str(animei.title), 'yellow', attrs=['bold'])
+  port = 5666
+
+  app = Flask(__name__)
+
+  app.logger.warning('')
+
+  @app.before_request
+  def middleware():
+    if request.endpoint != 'index':
+        return redirect('/')
+        
+  @app.route('/')
+  def index():
+    return render_template('index.html', title=animei.title, servers=servers, episode=animei.episode)
+  
+  episode = colored(str(animei.episode), 'cyan', attrs=['bold'])
+  title = colored(str(animei.title), 'yellow', attrs=['bold'])
     
-    # UI
-    clear()
-    print(f'¡Ahora puedes ver el episodio {episode} de {title}!')
-    printb('\nAbre este enlace en tu navegador:')
-    print(f'http://localhost:{port} \n')
-    
-    try:
-      httpd.serve_forever()
-    except KeyboardInterrupt:
-      httpd.server_close()
-      httpd.socket.close()
-    finally:
+  # UI
+  clear()
+  print(f'¡Ahora puedes ver el episodio {episode} de {title}!')
+  printb('\nAbre este enlace en tu navegador:')
+  print(f'http://localhost:{port} \n')
+  
+  if __package__ == 'ania.web_player':
+      app.run(port=port, debug=False)
       bye()
